d4f0983a1fb2eefde1a335437f96604569c52047
Fahad Hasan <hasan.fahad67@outlook.com>
2025-03-19 00:11:33 +0600

    First Code

---
+++ FirstCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.Write("Enter Your Name : ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Your Id : ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Details");
+            Console.WriteLine("Your id is = " + id);
+            Console.WriteLine("Your name is = " + name);
+
+        }
+    }
+}
